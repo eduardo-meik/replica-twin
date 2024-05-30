d66955a441257a422786b5bfe66b392e8e4f7bbe
eduardo-meik <eduardo@meiklabs.com>
2024-05-30 13:05:04 -0400

    FIRST PUSH

--- model/db.js
+++ model/db.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 class Database {
     async connect() {
         try {
-            await mongoose.connect(process.env.MONGODB_URL);
-            console.log('Database connection successfull.');
+            await mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+            console.log('Database connection successful.');
         } catch(err) {
             console.log('Database connection error:', err);
         }
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const helmet = require('helmet');
@@ -7,6 +8,10 @@ const { FORGE_CLIENT_ID, FORGE_CLIENT_SECRET, FORGE_MODEL_URN, MONGODB_URL } = p
 if (!FORGE_CLIENT_ID || !FORGE_CLIENT_SECRET || !FORGE_MODEL_URN || !MONGODB_URL) {
   console.warn('Provide all the following env. variables to run this application:');
   console.warn('FORGE_CLIENT_ID, FORGE_CLIENT_SECRET, FORGE_MODEL_URN, MONGODB_URL');
+  console.log(process.env.FORGE_CLIENT_ID);
+  console.log(process.env.FORGE_CLIENT_SECRET);
+  console.log(process.env.FORGE_MODEL_URN);
+  console.log(process.env.MONGODB_URL);
   return;
 }
 
